6bde24056a29866ccb0196a141efb5e9c0745868
Luis Passos <luis.passos@datacentric-tech.com>
2026-05-26 17:14:09 +0100

    feat(marp): register custom diagram plugin

--- bin/engine.mjs
+++ bin/engine.mjs
@@ -1,6 +1,7 @@
 import GanttPlugin from "/plugins/marp/gantt-diagram/index.js";
+import CustomDiagramPlugin from "/plugins/marp/custom-diagram/index.js";
 import hljs from "markdown-it-highlightjs";
 
 export default ({ marp }) => {
-  return marp.use(GanttPlugin).use(hljs);
+  return marp.use(GanttPlugin).use(CustomDiagramPlugin).use(hljs);
 };
